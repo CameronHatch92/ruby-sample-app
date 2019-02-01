4b2fbc2c1462529b69c99c98d806994c64066fa2
Cameron Hatch <cameronlavigne92@gmail.com>
2019-02-01 14:23:03 -0700

    set root to home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   # the below rule maps get requests to static_pages/home to the home action
   # in the Static Pages controller
+  root 'static_pages#home'
   get 'static_pages/home'
   get 'static_pages/help'
   get 'static_pages/about'
   get 'static_pages/contact'
-  root 'application#hello'
 end
