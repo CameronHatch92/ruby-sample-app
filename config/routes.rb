2946ebd8126b966b7116fa57db233bad13b9ff1c
Cameron Hatch <cameronlavigne92@gmail.com>
2019-02-01 13:34:53 -0700

    added html to help and home views

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  # the below rule maps get requests to static_pages/home to the home action
+  # in the Static Pages controller
+  get 'static_pages/home'
+  get 'static_pages/help'
   root 'application#hello'
 end
